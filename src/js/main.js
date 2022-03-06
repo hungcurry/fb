8f3a7b5b4e2fa080043e1e60aa2a3030d369752e
李佳鴻 <ooopp42@gmail.com>
2022-03-06 18:50:18 +0800

    init

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+//-------------------
+// 檔案
+//-------------------
+import scss from "all.scss";
+
+
+
+//-------------------
+// 插件
+//-------------------
+// tailwind css
+import tailwind from "tailwind.css";
+
+
+//---js---
+import axios from "axios";
+// babel/polyfill 主要檔案
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+
+
+
+
+
