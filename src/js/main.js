12b46683726ba445427e0038dbbb33abe32fc8db
李佳鴻 <ooopp42@gmail.com>
2022-03-07 23:48:01 +0800

    [ add ] header選單內容添加

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,33 @@ import "regenerator-runtime/runtime";
 
 
 
+const popoverBtn = document.querySelectorAll('.nav-btn');
+const popoverPanel = document.querySelectorAll('.popover-panel');
 
+function openPanel(index) {
+  popoverPanel.forEach( function(item , idx){
+    item.classList.add('hidden');
+    // 防止觸發到 window 事件 , 避免點擊 內容區塊關閉
+    item.addEventListener('click' , function(e) {
+      console.log(`popoverPanel`);
+      e.stopPropagation();
+    })
+    if(index === idx + 1) {
+      item.classList.remove('hidden');
+    }
+  })
+};
+function clickPopoverFn(e){
+  console.log('popoverBtn');
+  e.stopPropagation();
+  let id = Number(e.currentTarget.getAttribute('id').substr(3));
+  openPanel(id);
+};
 
-
+window.addEventListener('click' , function(e){
+  console.log('windows');
+  openPanel(-1);
+});
+popoverBtn.forEach( function(item){
+  item.addEventListener('click', clickPopoverFn)
+});
