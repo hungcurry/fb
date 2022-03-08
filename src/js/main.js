72b496b35630f158e3dcc537e09693e14b6954a6
李佳鴻 <ooopp42@gmail.com>
2022-03-08 23:49:30 +0800

    [ add ] 左側相關內容添加

--- src/js/main.js
+++ src/js/main.js
@@ -50,3 +50,79 @@ window.addEventListener('click' , function(e){
 popoverBtn.forEach( function(item){
   item.addEventListener('click', clickPopoverFn)
 });
+
+//  ------------- 左側相關 -------------
+const leftBlock = document.querySelector("#left-block");
+const leftArr = [
+  {
+    name: "白爛貓",
+    img: "https://media1.giphy.com/media/H1eshAZvAfiofm870b/giphy.webp",
+  },
+  {
+    name: "活動",
+    img: "https://bruce-fe-fb.web.app/image/left/activity.svg",
+  },
+  {
+    name: "天氣",
+    img: "https://bruce-fe-fb.web.app/image/left/cloudy.png",
+  },
+  {
+    name: "災害應變中心",
+    img: "https://bruce-fe-fb.web.app/image/left/dynamic.svg",
+  },
+  {
+    name: "新冠病毒資訊中心",
+    img: "https://bruce-fe-fb.web.app/image/left/facemask.svg",
+  },
+  {
+    name: "社團",
+    img: "https://bruce-fe-fb.web.app/image/left/friend.svg",
+  },
+  {
+    name: "企業管理平台",
+    img: "https://bruce-fe-fb.web.app/image/left/job.png",
+  },
+  {
+    name: "Messenger",
+    img: "https://bruce-fe-fb.web.app/image/left/messenger.svg",
+  },
+  {
+    name: "近期廣告動態",
+    img: "https://bruce-fe-fb.web.app/image/left/pay.png",
+  },
+  {
+    name: "朋友名單",
+    img: "https://bruce-fe-fb.web.app/image/left/sale.png",
+  },
+  {
+    name: "最愛",
+    img: "https://bruce-fe-fb.web.app/image/left/star.svg",
+  },
+  {
+    name: "Marketplace",
+    img: "https://bruce-fe-fb.web.app/image/left/store.svg",
+  },
+  {
+    name: "Watch",
+    img: "https://bruce-fe-fb.web.app/image/left/watchingTv.svg",
+  },
+];
+function renderLeftItem(name, imageUrl) {
+  const item = `
+    <div class="flex items-center justify-items-center w-full p-2 mb-6 rounded hover:bg-fb-input cursor-pointer">
+      <div class="w-[32px] overflow-hidden rounded-full mr-4">
+        <img class="object-cover" src="${imageUrl}" alt="" />
+      </div>
+      <p class="text-white text-2xl">${name}</p>
+    </div>
+  `;
+  return item;
+}
+function renderLeftList() {
+  let htmlStr = "";
+  leftArr.forEach((item) => {
+    htmlStr += renderLeftItem(item.name , item.img);
+  });
+  leftBlock.innerHTML = htmlStr;
+}
+renderLeftList();
